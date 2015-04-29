d70e85776dbc053fe2c73eb0e4e8fd8248c2691c
Keenan Lidral-Porter <klp2012@gmail.com>
2015-04-28 21:11:33 -0700

    forgot what this commit is for

---
+++ oldyeller/server.js
@@ -0,0 +1,27 @@
+// old yeller fetches the data, continuously
+
+var needle = require ('needle');
+var Promise = require('es6-promise').Promise;
+var Firebase = require('Firebase');
+var baseUrl = 'http://api.metro.net/agencies/lametro/routes/';
+var dataStore = new Firebase("https://lahack-transit.firebaseio.com");
+
+var options = {
+  follow_max: 5,    // follow up to five redirects 
+};
+ 
+var stream = needle.get(baseUrl, options);
+ 
+stream.on('readable', function() {
+    while (data = this.read()) {
+      var routes = data.items;
+      routes.forEach(function(route){
+        needle.get(baseUrl + route.id + '/vehicles/', function(error, response){
+          if(!error){
+            console.log(response.body);
+          }
+        });
+      });
+    }
+});
+
