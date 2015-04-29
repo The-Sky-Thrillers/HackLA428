60da27086dfbb5d2cbeb12ec868002651e0f7279
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-04-28 20:44:51 -0700

    file setup

---
+++ js/metro-api.js
@@ -0,0 +1,11 @@
+
+
+$.ajax({
+  url: "/api/getWeather",
+  data: {
+    zipcode: 97201
+  },
+  success: function( data ) {
+    $( "#weather-temp" ).html( "<strong>" + data + "</strong> degrees" );
+  }
+});
