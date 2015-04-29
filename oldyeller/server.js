a11975c11aba4e8e39b6a94766f8d7876d028c48
Keenan Lidral-Porter <klp2012@gmail.com>
2015-04-28 21:40:08 -0700

    adds all routes to firebase as empty arrays

--- oldyeller/server.js
+++ oldyeller/server.js
@@ -11,21 +11,15 @@ var options = {
 };
  
 var stream = needle.get(baseUrl, options);
-var route = [];
+var routes = {};
 
 stream.on('readable', function() {
-    while (data = this.read()) {
-      var routes = data.items;
-      routes.forEach(function(route){
-        needle.get(baseUrl + route.id + '/vehicles/', function(error, response){
-            route.push({
-              id: response.body.id,
-              latitude: response.body.latitude,
-              longitude: response.body.longitude
-            });
-          }
-        });
-      });
-    }
+  while (data = this.read()) {
+    var items = data.items;
+    items.forEach(function(route){
+      routes[route.id] = [];
+    });
+    dataStore.set(routes);
+  }
 });
 
